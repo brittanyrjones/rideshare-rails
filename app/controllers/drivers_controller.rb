c0fd15094612a65fbf1f6625fcb1e0d3a87d6182
Brittany Jones <brittany.r.jones@hotmail.com>
2018-04-03 14:25:26 -0700

    updated driver controller methods

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,22 +1,51 @@
 class DriversController < ApplicationController
   def index
+    @drivers = Driver.all
   end
 
   def new
+    @driver = Driver.new
   end
 
   def create
+    @driver = Driver.new
+    if @driver.save
+      redirect_to driver_path
+    else
+      render :new
+    end
   end
 
   def update
+    @driver = Driver.find_by(id: params[:id])
+    if !@driver.nil?
+      if @driver.update
+        redirect_to driver_path(@driver.id)
+      else
+        render :edit
+      end
+    else
+      redirect_to driver_path
+    end
   end
 
+
   def edit
+    @driver = Driver.find_by(id: params[:id])
   end
 
   def show
+    id = params[:id]
+    @driver = Driver.find(id)
   end
 
-  def delete
+  def destroy
+    id = params[:id]
+    @driver = Driver.find(id)
+    if @driver
+      @driver.destroy
+    end
+    redirect_to drivers_path
   end
+
 end
