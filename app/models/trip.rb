5a83f9234434c2dac75cc27a10610f9e117dfb79
Phoebe <phoebe.manning8@gmail.com>
2018-04-04 18:58:28 -0700

    fixes validatation syntax error

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
-  
-  validates: :rating, numericality: {less_than_or_equal_to: 5}, message: "rating must be less than or equal to 5"
+
+  validates_numericality_of :rating, :less_than_or_equal_to => 5, :message => "rating must be less than or equal to 5"
 end
